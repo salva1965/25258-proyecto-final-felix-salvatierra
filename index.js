5c049241a8aeea3ef197c463985b1daf7aa8e473
nest <salvatierra.felix.rogelio@gmail.com>
2025-12-03 14:08:46 -0300

    cambios 4

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+import cors from "cors";
 import "dotenv/config";
 import express from "express";
 import productRouter from "./src/routes/productRouter.js";
 
 const app=express();
+
+app.use(cors());
 
 app.use(express.json());
 
